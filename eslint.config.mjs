d81c26059278a69135b97baec0c8c9c9681980b4
mdaushi <firdaus.ex19@gmail.com>
2025-04-01 01:29:13 +0800

    Disable unused variable warnings in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // Menonaktifkan peringatan variabel yang tidak digunakan
+    },
+  },
 ]
 
 export default eslintConfig
